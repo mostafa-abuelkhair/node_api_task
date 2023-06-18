dd0d6d54337ca9df4184cf52f89f5bec29b0c564
mostafa ahmed <mostafa.ahmed2509@gmail.com>
2023-06-18 04:15:27 +0300

    make api to get curr and post product

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
+let http = require('http');
+let url = require('url');
+let yup = require('yup');
+
+const productScheme = yup.object({
+        title: yup.string().required(),
+        price: yup.number().required(),
+        description: yup.string().required(),
+        categoryId: yup.number().required(),
+        images: yup.array()
+  });
+
 let categorized = [];
 
- const getData = async () => {
+const getData = async (req,resUser) => {
    try {
 
         const res = await fetch("https://api.escuelajs.co/api/v1/products");
@@ -16,18 +28,95 @@ let categorized = [];
         });
 
 
-       const res2 = await fetch("https://api.exchangerate.host/latest?base=USD");
+        const cur = url.parse(req.url, true).query.cur;
 
-       const data = await res2.json();
+        
+        const res2 = await fetch("https://api.exchangerate.host/latest?base=USD");
 
-       const rate = data.rates.EGP;
+        const data = await res2.json();
 
-       categorized.forEach( c => { c.products.forEach( p=>{ p.price = p.price*rate } ) }  );
+        const rate = data.rates[cur];
 
-       console.log(categorized);
-    
+        if (rate === undefined){resUser.end("currency not found")}
+        else {
+            categorized.forEach( c => { c.products.forEach( p=>{ p.price = p.price*rate } ) }  );
+
+            resUser.setHeader("Content-Type", "application/json");
+            resUser.end(JSON.stringify(categorized));
+         }
+
    }
     catch (error) {console.log(error)}
 };
 
-getData();
+
+const postProduct =  async (product,resUser)=>{
+
+    const res = await fetch("https://api.escuelajs.co/api/v1/products",{
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(product)
+      });
+
+    const product_response = await res.json();
+
+    resUser.setHeader("Content-Type", "application/json");
+    resUser.end( JSON.stringify(product_response) );
+
+}
+
+
+
+
+const server = http.createServer((req, res) => {
+
+    const path = url.parse(req.url, true).pathname;
+
+    if (path==="/products"){
+
+        const method = req.method;
+        
+        if (method==="GET"){ getData(req,res); }
+
+        else if(method==="POST"){
+
+            const chunks = [];
+            req.on("data", (chunk) => {
+              chunks.push(chunk);
+            });
+
+            req.on("end", () => {
+                try{
+
+                    const product = productScheme.validateSync(JSON.parse(chunks.toString()), {
+                        strict: true,
+                      });
+        
+                    postProduct(product,res); 
+   
+                }
+                catch(error){
+                    res.writeHead(400);
+                    res.end(error.toString());
+                }
+    
+            });
+
+        }
+        
+    }
+    else{
+        res.writeHead(404);
+        res.end("path not found, use /products to get or post product ");
+    }
+
+
+})
+
+
+server.listen(8080, () => {
+    console.log("SERVER is running on http://localhost:8080");
+  });
